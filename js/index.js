dc8bce920fa15d0797af2c77b25bb89e8fc782a3
gzachr <zachary_gomez@dlsu.edu.ph>
2023-06-23 22:54:01 +0800

    yellow bg for post, edited index pages

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 //change comment icon on posts on hover
 function changeComment(x) {
-    x.classList.toggle("bi-chat-left-fill");
-    x.classList.toggle("bi-chat-left"); 
+  x.classList.toggle("bi-chat-left-fill");
+  x.classList.toggle("bi-chat-left"); 
+}
+
+function changeUpvote(x) {
+  x.classList.toggle("upvote");
+  x.classList.toggle("upvote-2");
+}
+
+function changeDownvote(x) {
+  x.classList.toggle("downvote");
+  x.classList.toggle("downvote-2");
 }
 
 //quill library for rich text-editor
