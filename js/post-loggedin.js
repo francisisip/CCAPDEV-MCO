42b10cfd0595d7ee48eece7b402b6d1b2ba052e4
gzachr <zachary_gomez@dlsu.edu.ph>
2023-06-25 22:20:40 +0800

    added create post modal to post

--- js/post-loggedin.js
+++ js/post-loggedin.js
@@ -22,26 +22,3 @@ function updateTextarea() {
 
 // Add event listener to update the textarea when content is changed
 quill.on('text-change', updateTextarea);
-
-//for image uploading
-const uploadInput = document.getElementById('upload-input');
-const imagePreview = document.querySelector('.image-preview');
-
-uploadInput.addEventListener('change', (e) => {
-const files = e.target.files;
-if (files && files.length > 0) {
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    if (file.type.startsWith('image/')) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const imagePreviewItem = document.createElement('div');
-        imagePreviewItem.classList.add('image-preview-item');
-        imagePreviewItem.style.backgroundImage = `url('${event.target.result}')`;
-        imagePreview.appendChild(imagePreviewItem);
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-}
-});
